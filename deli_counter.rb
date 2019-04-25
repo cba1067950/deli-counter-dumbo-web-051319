314ee7154c82c53ecf43c503b9b9ca62e75d52a8
Joe Jamsky <cba1067950@yahoo.com>
2019-04-25 22:32:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,13 +3,10 @@ katz_deli = []
 
 def line(array)
   linePosition = []
+  
   if array.length == 0 
     puts "The line is currently empty."
-  else
-    array.each_with_index do |item, index|
-      #linePosition.push("#{index +1}. #{item}")
   end
-  puts linePosition
 end
 
 def take_a_number()
