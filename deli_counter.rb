c7ca89335fcc30903d249df686dda2c7bcd36b3c
Joe Jamsky <cba1067950@yahoo.com>
2019-04-25 22:26:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,12 @@
 # Write your code here.
 katz_deli = []
 
-def line()
-  
+def line(array)
+  if array.length == 0 
+    "The line is currently empty."
+  else
+    #array.each_with_index do |item, index|
+    puts array
 end
 
 def take_a_number()
